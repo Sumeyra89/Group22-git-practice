8dd3ea5f92e426cff337200472c90e81e38231c1
Sumeyra89 <72941277+Sumeyra89@users.noreply.github.com>
2021-02-13 12:49:48 -0800

    feature c branch work is done

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World group22");
         System.out.println("group22 future sdet");
     }
 }
