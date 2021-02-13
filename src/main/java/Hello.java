3d635216d953da6aaf31fba79af45cb0813db1e3
Sumeyra89 <72941277+Sumeyra89@users.noreply.github.com>
2021-02-13 12:59:26 -0800

    nice if you

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World group22");
-        System.out.println("group22 future sdet");
+        System.out.println("group22 future ");
         //watching more movie
     }
 }
--- src/test/java/Movie.java
+++ src/test/java/Movie.java
@@ -1,6 +1,6 @@
 public class Movie {
     public static void main(String[] args) {
         //what a conflict
-        //nice if you done
+        //nice if you
     }
 }
