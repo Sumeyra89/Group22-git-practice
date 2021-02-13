5f1f5b6723677a6d2c4704719bfffb6fc23239d7
Sumeyra89 <72941277+Sumeyra89@users.noreply.github.com>
2021-02-13 11:42:18 -0800

    added new commit featurea 2

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -1,5 +1,6 @@
 public class FeatureACode {
     public static void main(String[] args) {
         System.out.println("Awesome");
+        System.out.println("group22 is learning git with Akbar");
     }
 }
